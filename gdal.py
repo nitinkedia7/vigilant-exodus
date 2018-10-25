12759dd9784e50b1191dd56c6929509936e7a78c
Nitin Kedia <nitinkedia7@gmail.com>
2018-10-25 10:52:52 +0530

    Debug Test11: use apt-get instead of apt

--- gdal.py
+++ gdal.py
@@ -8,5 +8,5 @@ def subprocess_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print (proc_stdout.decode("utf-8"))
 
-subprocess_cmd('apt install -y binutils libproj-dev gdal-bin')
+subprocess_cmd('apt-get install -y binutils libproj-dev gdal-bin')
 subprocess_cmd('GUNICORN_CMD_ARGS="--bind=0.0.0.0 --timeout 600" gunicorn exodus.wsgi')
